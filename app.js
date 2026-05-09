299430128f5e11a18ccf10f36e05f365cbdc3dc8
Mohamedhegazy93 <132653963+Mohamedhegazy93@users.noreply.github.com>
2026-05-10 00:48:44 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello World! Pipeline is Working!'));
+app.get('/', (req, res) => res.send('Hello World! Pipeline is Working22!'));
 app.listen(3000, () => console.log('Server is running on port 3000'));
